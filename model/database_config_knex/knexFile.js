c199c91e1db75594a3729cb35c070e841102ceef
Jean Costa <jc090504@gmail.com>
2026-06-09 10:58:35 -0300

    Ajustando database

--- model/database_config_knex/knexFile.js
+++ model/database_config_knex/knexFile.js
@@ -6,7 +6,7 @@ module.exports = {
         host: 'localhost', // Substitua pelo seu host, se necessário
         user: 'root', // Substitua pelo seu usuário
         password: 'bcd127', // Substitua pela sua senha
-        database: 'db_filmes_2026_a',
+        database: 'db_delicia_gelada', // Substitua pelo nome do seu banco de dados
         port: 3306, // Porta padrão do MySQL
        
         // Opcional: Define charset (recomendado para UTF8)
